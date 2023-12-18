9b97e7cc5f26d11944a984b90854575af7c56728
minsun309 <pminsun309@gmail.com>
2023-12-18 16:10:33 +0900

    Open Graph 오류 해결 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { SCoreDreamFont } from "@/styles/Fonts";
+import Head from "next/head";
 
 //Add custom appProp type then use union to add it
 type CustomAppProps = AppProps & {
@@ -41,25 +42,57 @@ export default function App({
   });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        {Component.auth ? (
-          <Auth>
+    <>
+      <Head>
+        <title>크리스마스 파티 초대장</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Minsun" />
+        <meta
+          name="description"
+          content="12/23-24 크리스마스 파티 초대장 입니다."
+        />
+        <meta property="og:title" content={"크리스마스 파티 초대장"} />
+        <meta property="og:site_name" content="크리스마스 파티" />
+        <meta
+          property="og:description"
+          content={"12/23-24 크리스마스 파티 초대장 입니다."}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={"https://christmas-invitation-pminsun.vercel.app/"}
+        />
+        <meta
+          property="og:image"
+          content={
+            "https://images.unsplash.com/photo-1576995083066-f7bf549dc771?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:article:author" content="MinSun" />
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider session={session}>
+          {Component.auth ? (
+            <Auth>
+              <LayoutComponent>
+                <Hydrate state={pageProps.dehydratedState}>
+                  <Global styles={[reset, SCoreDreamFont]} />
+                  <Component {...pageProps} />
+                </Hydrate>
+              </LayoutComponent>
+            </Auth>
+          ) : (
             <LayoutComponent>
-              <Hydrate state={pageProps.dehydratedState}>
-                <Global styles={[reset, SCoreDreamFont]} />
-                <Component {...pageProps} />
-              </Hydrate>
+              <Global styles={[loginReset, SCoreDreamFont]} />
+              <Component {...pageProps} />
             </LayoutComponent>
-          </Auth>
-        ) : (
-          <LayoutComponent>
-            <Global styles={[loginReset, SCoreDreamFont]} />
-            <Component {...pageProps} />
-          </LayoutComponent>
-        )}
-      </SessionProvider>
-    </QueryClientProvider>
+          )}
+        </SessionProvider>
+      </QueryClientProvider>
+    </>
   );
 }
 
